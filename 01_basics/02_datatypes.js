a0af3ec5f5d5cf617dd32c955e8b3b6546e0bf4f
Rahul Sah <rahul.241226@ncit.edu.np>
2026-07-19 12:40:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";// treat all js code as newer version 
+
+//alert(3+3)//we are using node.js aot browser
+
+console.log(3+3); 
+console.log("rahul sah")
+
+let name="rahul"
+let age = 56
+let isloggedin= false
+
+
+//number>=2 to power 53,,,
+//bigint 
+//string=>""
+//boolean = true or false 
+//null => standalone value
+// undefined => yet value is not defined
+//symbol : to find uniqueness
+
+
+//objects
+
+
+console.log(typeof"rahul")
+
+console.log(typeof null)// return objects 
+console.log(typeof undefined) //returns undefined 
