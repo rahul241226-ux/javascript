af9593338eec46efbbf8aefcd7c4f09671ede1bd
Rahul Sah <rahul.241226@ncit.edu.np>
2026-07-19 13:03:51 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,21 @@
+
+//let score = 44
+// let score = "44"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// // let valueinnumber = Number (score)
+// console.log(typeof valueinnumber);
+
+// let isloggedin = 1 
+//let isloggedin = "asdfghj" 
+let isloggedin = "" 
+
+
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);// 0 = false , 1 = true and string"rahul sah" = true and empty string = false
+
+let somenumber =33
+let stringnumber = String(somenumber);//converted const into string.
+console.log(stringnumber);
+console.log(typeof stringnumber);
