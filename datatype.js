87a232355e30a765c657b7920ea7ba0100ef6b89
Rahul Sah <rahul.241226@ncit.edu.np>
2026-07-19 14:42:42 +0000

    datatypes

---
+++ datatype.js
@@ -0,0 +1,52 @@
+//primitive
+//7 types: String,Number,Boolean,Null(emptty),undefined,Symbol,BigInt
+
+
+
+
+
+// Reference (Non Primitive)
+
+//Array,Objects, functions
+
+
+const score =100
+const scorevalue = 100.3
+
+const isloggedIn = false
+const outsideTemp = null
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id == anotherid)// compare variables and return boolean 
+
+
+
+ //const bigNumber = 123456789784567890678
+//objects
+const heros = ["thor","ironman","loki"]// return strings
+let myObj = {
+
+name :"rahul",
+age:22,
+
+
+}// return what in the bracket 
+
+console.log(heros);
+console.log(myObj);
+
+
+
+//function
+
+const myFunction = function(){
+
+console.log("Hello");
+
+
+}
+
+console.log(typeof bigNumber);//if not defined then undefined
+console.log(typeof myFunction);//returns function
+console.log(typeof hero);//return undefined
