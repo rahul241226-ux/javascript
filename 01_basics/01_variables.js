36bd849e149a7902c76742968f4a44dc4d8da67e
Rahul Sah <rahul.241226@ncit.edu.np>
2026-07-19 12:12:36 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId  = 1234
+let accountEmaill ="rahul@google.com"
+var accounntPassword = "3456"
+accountcity = "gaushala"// in js we can declare variable withouut datatype but not safe 
+let accountstate ;
+
+//if there is const let var then assign them inside memory 
+//const declared as permanaent 
+///accountId = 2 //= is not allowed
+
+accountEmaill = "sdfghjk"
+accounntPassword="345678"
+accountcity="dfghj"
+
+console.log(accountId);
+console.table([accountId,accountEmaill,accounntPassword,accountId,accountstate])
+
+//so only use const and let ,never use var because of issue in block and functional scope
+
