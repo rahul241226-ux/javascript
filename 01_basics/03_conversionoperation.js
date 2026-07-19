812c4b5cfa4afd79ff4ad4c0fb1d45b7c9397a6f
Rahul Sah <rahul.241226@ncit.edu.np>
2026-07-19 13:51:42 +0000

    operation on datatype

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -19,3 +19,66 @@ let somenumber =33
 let stringnumber = String(somenumber);//converted const into string.
 console.log(stringnumber);
 console.log(typeof stringnumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------------opertaion------------------//
+
+
+let value =3 ;
+let negvalue = - value
+console.log(negvalue);
+
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+
+let str1 = "hello"
+let str2 = " rahul"
+
+let str3 = str1+str2;
+console.log(str3);
+
+
+
+// console.log(1+2);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"3");
+
+
+console.log(true+true);//1+1=2
+console.log(true+false);//1+0=1
+
+
+let num1, num2, num3
+
+num1 = num2 =num3 = 2+2
+console.log(num1)
+
+
+let gamecounter = 100;
+//++gamecounter ;
+gamecounter++;
+console.log(gamecounter);
