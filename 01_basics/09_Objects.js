2665f0d9d31bb8f1fdf136f396876a625ebde0ed
Rahul Sah <rahul.241226@ncit.edu.np>
2026-07-21 02:55:18 +0000

    objects

--- 01_basics/09_Objects.js
+++ 01_basics/09_Objects.js
@@ -32,14 +32,14 @@ isloggedin:false,
 // console.log(Jsuser.email);
 // console.log(Jsuser["email"]);
 // console.log(Jsuser["full name"]);
-// console.log(typeof Jsuser.mySym)
+// console.log(typeof Jsuser.[mySym])
 
 
 
 Jsuser.email= "rahulsah@chatgptgmail.com"
 //Object.freeze(Jsuser);
 Jsuser.email= "rahulsah@google.com"
-console.log(Jsuser);
+// console.log(Jsuser);
 
 
 
@@ -47,10 +47,149 @@ console.log(Jsuser);
 Jsuser.greeting = function(){
 
 ///console.log("hello JS user");
-console.log(`hello JS user,${this.name}`);//string interpolation
+// console.log(`hello JS user,${this.name}`);//string interpolation
 
 
 
 
 }
-console.log(Jsuser.greeting());
+// console.log(Jsuser.greeting());
+
+
+
+
+
+
+
+//-------------------part 2 Objects ---------------------
+
+
+
+const tinder = new Object()// singleton objects 
+//const tinder = {}// non singelton object 
+
+ tinder.id = "123ab"
+tinder.name ="chandan"
+tinder.isloggedin = false
+
+//console.log(tinder);
+
+const regularuser = {
+
+
+email : "some@gmail.com",
+fullname:{
+
+
+userfullname :{
+
+
+firstname:"rahul",
+lastname:"sah",
+
+}
+
+}
+
+
+
+}
+
+//console.log(regularuser.fullname?.userfullname.firstname)
+
+const obj1 ={1:"a", 2:"b"}
+const obj2 = {3:"a",4:"b"}
+
+
+// const obj3 = Object.assign({},onj1,obj2)
+const obj3 = {...obj1,...obj2};
+// console.log(obj3)
+
+
+
+
+const users = [
+
+{
+
+id:1,
+email:"rahulsah@gmail.com",
+
+},
+{
+
+id:1,
+email:"rahulsah@gmail.com",
+
+},
+
+{
+
+id:1,
+email:"rahulsah@gmail.com",
+
+},
+]
+
+users[1].email
+// console.log(tinder);
+
+// console.log(Object.keys(tinder));
+// console.log(Object.values(tinder));
+// console.log(Object.entries(tinder));
+
+// console.log(tinder.hasOwnProperty('isloggedin'));
+
+
+//----------objects destructing and JSON API---------------
+
+
+
+
+
+//DESTRUCTING-----------------
+
+
+
+const course ={
+
+coursename :"js ",
+price:23,
+courseteacher:"rahul",
+
+
+
+
+}
+
+const{courseteacher:teacher}=course;// this is how we destructure objects 
+console.log(teacher)
+
+
+
+
+const navbar =({company}) =>{
+
+
+
+
+}
+navbar(company ="rahul")
+
+// {
+
+// "name":"rahul".,
+// "course":"javascript",
+// "price":"free",
+// }
+
+
+
+[
+
+{},
+{},
+{},
+
+
+]
