8a1dfefa4e0d3df53f2b954063d36ec60f039a89
Rahul Sah <rahul.241226@ncit.edu.np>
2026-07-23 05:20:10 +0000

    filter

---
+++ 01_basics/16_filtermap.js
@@ -0,0 +1,56 @@
+// const coding =["js","ruby","java","python","cpp"]
+
+// const values= coding.forEach((item)=>{
+
+
+// // console.log(item)
+// return item;
+
+
+// })
+// console.log(values);
+
+
+//---------------------------------
+
+
+
+
+const num =[1,2,3,4,5,6,7]
+
+const newNumber=num.filter((number) => number  >4)
+
+// console.log(newNumber);
+
+
+
+
+
+
+//---------------------------------
+
+
+
+//  const myNum =[1,2,3,4,5,6,7,8,9]
+// const number = myNum.map((num) => num+10)
+// console.log(number);
+
+
+
+
+
+//------------------Multiple map and filter-----------------------------
+
+
+const newNums= myNum
+                    .map((num) => num*10)
+                    .map((num) => num+1)
+                    .filter((num) => num >=40)
+// console.log(newNums);
+
+
+//----------------------------Reduce-------------------
+
+
+
+
